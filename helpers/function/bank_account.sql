c71e11410dff6674f6260528f71230122f9b4b8c
Martin Czarkowski <58852990+martinczarkowski@users.noreply.github.com>
2020-05-29 15:06:55 +0200

    syntax editing

--- helpers/function/bank_account.sql
+++ helpers/function/bank_account.sql
@@ -13,19 +13,19 @@ RETURNS text
 AS
 $bank_account$
 DECLARE
-		l_prefix text;
-		l_bank_account text;
+	l_prefix text;
+	l_bank_account text;
 BEGIN
-		IF helpers.empty(in_bank_prefix) THEN l_prefix := null;
-			  ELSE l_prefix := concat (in_bank_prefix, '-');
-		END IF;
+	IF helpers.empty(in_bank_prefix) THEN l_prefix := null;
+		ELSE l_prefix := concat (in_bank_prefix, '-');
+	END IF;
 
-		IF helpers.empty(in_bank_account) THEN l_bank_account := null;
-				ELSE l_bank_account := concat(l_prefix, in_bank_account, '/', in_bank_code_ident)::text;
-		END IF;
-
+	IF helpers.empty(in_bank_account) THEN l_bank_account := null;
+		ELSE l_bank_account := concat(l_prefix, in_bank_account, '/', in_bank_code_ident)::text;
+	END IF;
 RETURN l_bank_account;
 END;
+
 $bank_account$
 LANGUAGE PLPGSQL
 STABLE
